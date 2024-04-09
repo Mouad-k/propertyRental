63caf23d0195b549641d73b5f2b8ee57f12d3adb
Mouad-k <mouadkhallati1@gmail.com>
2024-04-09 17:11:07 +0000

    project created

---
+++ src/test/java/com/project/propertyRental/PropertyRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.propertyRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
